4a4d7739c50e0a41f4445b5d04cdfe69124ebf75
Ubuntu <ubuntu@server-2.novalocal>
2014-12-03 02:59:08 +0000

    bug fixed

--- liveStreaming/views.py
+++ liveStreaming/views.py
@@ -118,7 +118,7 @@ def noviewer(request):
 
 @csrf_exempt
 def error(request):
-    queryDict = request.GET
+    queryDict = request.POST
     appName = queryDict.__getitem__(CONFIG['appname'])
     streamName = queryDict.__getitem__(CONFIG['stream'])
     rootStatus = queryDict.__getitem__(CONFIG['rootStatus'])
